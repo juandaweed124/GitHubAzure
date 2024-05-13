6cfd2bfb74649bcfc53dc61eab6126a297c8cb5d
juandaweed124 <juandaweed124@gmail.com>
2024-05-13 17:31:24 -0500

    cambio en config headless true

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
 
 
   use: {
-    headless: false,
+    headless: true,
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
